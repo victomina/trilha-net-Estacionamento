d385d58fabfdf2d4042029c60d435176c00e3476
victomina <victomina@gmail.com>
2023-04-10 23:36:46 -0300

    little changes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class ProgramHelloWorld
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
